c851759e413e8f21561dfdaf082f010d02f44c3b
kapeesh0103 <sharmakaps8077@gmail.com>
2023-12-05 21:07:39 +0530

    first commit

---
+++ contracts/contract.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+contract crowdFunding{
+function get(uint a) public pure returns(uint){
+   return a*2;
+}
+function get1(uint a) public pure returns (uint){
+    return 3*a;
+}
+
+
+}
